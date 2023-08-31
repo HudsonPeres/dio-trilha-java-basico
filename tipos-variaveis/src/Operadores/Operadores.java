067b94b77605f226a8ae8509ac432164cb17975f
HudsonPeres <hudsonaap@gmail.com>
2023-08-31 22:24:56 +0100

    Logicos 31/08/2023

--- tipos-variaveis/src/Operadores/Operadores.java
+++ tipos-variaveis/src/Operadores/Operadores.java
@@ -1,11 +1,29 @@
 package Operadores;
 public class Operadores {
     public static void main(String[] args) {
-        
-        String concatenacao ="?"; 
+      
+      String nomeUM = "Hudson";
+      String nomeDois = "Hudson";
+      System.out.println(nomeUM == nomeDois);
+      
+      
+        int numero1 = 2;
+      int numero2 = 2;
 
-        concatenacao = (1+1+1)+" Anos";
+        boolean simNao = numero1 == numero2;
 
-       System.out.println(concatenacao);
+        System.out.println("numeroUm é igual a numeroDois? " + simNao);
+
+        simNao = numero1 != numero2;
+
+        System.out.println("numeroUm é diferente a numeroDois? " + simNao);
+
+        simNao = numero1 > numero2;
+
+        System.out.println("numeroUm é maior a numeriDois? " + simNao);
+
+
+
+      
     }   
 }
--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,11 +1,8 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
        
-    short numeroCurto = 1;
-    int numeroNormal = numeroCurto;
-    short numeroCurto2 = (short) numeroNormal;
-
-    System.out.println(numeroNormal);
-       
+    int numero = 5;
+        numero = 10;
+        System.out.println(numero);
     }
 }
