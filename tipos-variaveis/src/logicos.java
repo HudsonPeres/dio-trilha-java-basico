77ac3eea1b69c648b0aa9e78ef6c893ada128b20
HudsonPeres <hudsonaap@gmail.com>
2023-09-01 17:08:55 +0100

    Aula metodos 01/09/2023

--- tipos-variaveis/src/logicos.java
+++ tipos-variaveis/src/logicos.java
@@ -6,8 +6,8 @@ public class logicos {
 
         boolean condicao2=false;
 
-        if(condicao1 && condicao2) {
-            System.out.println("as duas condições são verdadeiras");
+        if(condicao1 || condicao2) {
+            System.out.println("uma das duas condições é verdadeiras");
         }
         System.out.println("fim");
 
