e6a0b8072e65136b5c929fdf4abccc892ed1f782
HudsonPeres <hudsonaap@gmail.com>
2023-09-04 19:52:20 +0100

    Aula dia 04/09/2023

---
+++ controle-fluxo/src/BreakContinue.java
@@ -0,0 +1,16 @@
+public class BreakContinue {
+
+    public static void main(String[] args) {
+        
+        for(int numero = 1; numero <=5; numero ++){
+            if(numero==4)
+                continue;
+            
+            System.out.println(numero);
+            
+        }
+        //Qual a saída no console ?
+
+
+    }
+}
